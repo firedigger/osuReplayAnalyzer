bfb91081d81ecee193d5799697cc5b89004a4c71
Ivan Donat Pupovac <ivandonat@gmail.com>
2016-07-22 19:29:23 +0200

    Moved printing replay info into ReplayAnalyzer

--- osuDodgyMomentsFinder/Program.cs
+++ osuDodgyMomentsFinder/Program.cs
@@ -8,41 +8,6 @@ namespace osuDodgyMomentsFinder
 {
     class Program
     {
-
-        static void MainInfo(ReplayAnalyzer analyzer)
-        {
-            Console.WriteLine("\nGENERIC INFO");
-            Console.WriteLine("Unstable rate = " + analyzer.unstableRate());
-            Console.WriteLine("The best CS value = " + analyzer.bestCSValue());
-
-            Console.WriteLine("\nPIXEL PERFECT");
-
-            var pixelPerfectHits = analyzer.findSortedPixelPerfectHits(10);
-
-            Console.WriteLine("The best pixel perfect hit " + analyzer.findBestPixelHit());
-            Console.WriteLine("Pixel perfect hits: " + pixelPerfectHits.Count);
-            foreach (var hit in pixelPerfectHits)
-            {
-                Console.WriteLine("* " + hit.Key + " at " + hit.Value + "ms");
-            }
-
-
-            //Console.WriteLine(analyzer.hits.Count);
-
-            /*int i = 0;
-            foreach(var hit in analyzer.hits)
-            {
-                ++i;
-                if (i > 10)
-                    break;
-                Console.WriteLine(Utils.printInfo(hit.Value, hit.Key));
-            }*/
-
-            //beatmap.Save("new.osu");
-
-        }
-
-
         static void Main(string[] args)
         {
             if(args.Length == 0)
@@ -55,16 +20,10 @@ namespace osuDodgyMomentsFinder
             Console.WriteLine("REPLAY: " + args[1]);
 
             ReplayAnalyzer analyzer = new ReplayAnalyzer(beatmap, replay);
-
-            MainInfo(analyzer);
-
-            var overAims = analyzer.findOverAimHits();
-            Console.WriteLine("\nOver-aimed hits " + overAims.Count);
-            foreach (var hit in overAims)
-            {
-                Console.WriteLine("at " + hit + "ms");
-            }
-
+            analyzer.PrintMainInfo();
+            analyzer.PrintPixelPerfect();
+            analyzer.PrintOveraims();
+            
             Console.ReadKey();
         }
         
