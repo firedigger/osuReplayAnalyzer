2f30ef459c5c40e6056255ce81162eb43e8824a8
shavitush <djshavit@gmail.com>
2016-09-28 04:29:33 +0300

    Remove icon.

--- WPF_GUI/Properties/Resources.Designer.cs
+++ WPF_GUI/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace GUI.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
-        /// </summary>
-        internal static System.Drawing.Icon osu {
-            get {
-                object obj = ResourceManager.GetObject("osu", resourceCulture);
-                return ((System.Drawing.Icon)(obj));
-            }
-        }
     }
 }
