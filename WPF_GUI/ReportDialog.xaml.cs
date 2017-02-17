32b084031a1f7e57245c60f16c7106ad375bb4c6
shavit <shavitush@users.noreply.github.com>
2017-02-17 14:13:55 +0200

    New design, backend changes (#14)
    * New design, big backend changes
    
    * Capitalization changes
    
    * Minor polish
    
    * Move labels

--- WPF_GUI/ReportDialog.xaml.cs
+++ WPF_GUI/ReportDialog.xaml.cs
@@ -1,27 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-
-namespace WPF_GUI
+﻿namespace WPF_GUI
 {
 	/// <summary>
 	/// Interaction logic for ReportDialog.xaml
 	/// </summary>
-	public partial class ReportDialog : Window
+	public partial class ReportDialog
 	{
-		public ReportDialog()
+		public ReportDialog(string title)
 		{
-			InitializeComponent();
+		    Title = title;
+
+            InitializeComponent();
 		}
 	}
 }
